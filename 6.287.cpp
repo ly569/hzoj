5d6bb274f14250a276d42a32a8032b87bedf59fa
ly569 <58296803+ly569@users.noreply.github.com>
2020-08-03 19:34:06 +0800

    hzoj

---
+++ 6.287.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+#include <set>
+
+typedef pair<int, int> PII;
+
+int main() {
+    int n, a, t = 0;
+    int sum = 0;
+    cin >> n;
+    set<PII> s;
+    for (int i = 0; i < n; i++) {
+        cin >> a;
+        s.insert(PII(a, t++));
+    }
+    for (int i = 0; i < n - 1; i++) {
+        int a = s.begin()->first;
+        s.erase(s.begin());
+        int b = s.begin()->first;
+        s.erase(s.begin());
+        sum += a + b;
+        s.insert(PII(a + b, t++));
+    }
+    cout << sum << endl;
+    return 0;
+}
+
